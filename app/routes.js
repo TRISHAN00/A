13e0d0c31d9b8c49bbaad79f43e3800bad723710
TRISHAN00 <trishansaha43@gmail.com>
2022-10-30 01:44:56 +0600

    routes

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,12 @@
 const router = require("express").Router();
 
+router.use("/api/v1/tickets", require("../routes/ticketRoutes"));
+
 router.get("/health", (_req, res) => {
   //   const error = new Error("Hello Error");
   //   error.status = 400;
   //   throw error;
   res.status(200).json({ message: "success" });
-});
-
-router.get("/help", (_req, res) => {
-  res.status(200).json({ message: "Help" });
 });
 
 module.exports = router;
---
+++ routes/ticketRoutes.js
@@ -0,0 +1,16 @@
+const myDB = require("../db/MyDB");
+const router = require("express").Router();
+
+router.get("/t/:ticketId", () => {});
+router.patch("/t/:ticketId", () => {});
+router.delete("/t/:ticketId", () => {});
+
+router.get("/u/:username", () => {});
+router.patch("/u/:username", () => {});
+router.delete("/u/:username", () => {});
+
+router.post("/sell", () => {});
+router.post("/bulk", () => {});
+router.post("", () => {});
+
+module.exports = router;
