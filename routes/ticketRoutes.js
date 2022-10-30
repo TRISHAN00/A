550aaeb61eaa06805c02e6ef623c51f382a227b1
TRISHAN00 <trishansaha43@gmail.com>
2022-10-30 12:42:17 +0600

    routes with bulk tickets

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -9,10 +9,18 @@ router.get("/u/:username", () => {});
 router.patch("/u/:username", () => {});
 router.delete("/u/:username", () => {});
 
-router.post("/sell", () => {
-    
+router.post("/sell", (req, res) => {
+  const { username, price } = req.body;
+  const ticket = myDB.create(username, price);
+  res.status(201).json({ message: "Ticket created successfully", ticket });
 });
-router.post("/bulk", () => {});
+router.post("/bulk", (req, res) => {
+  const { username, price } = req.body;
+  const tickets = myDB.bulkCreate(username, price);
+  res
+    .status(201)
+    .json({ message: "Bulk Ticket created successfully", tickets });
+});
 router.post("", () => {});
 
 module.exports = router;
